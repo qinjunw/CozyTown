c9d36305412f147a3acf80a4a74338c3e3392593
Kaiisa <57869888+qinjunw@users.noreply.github.com>
2026-08-30 15:41:12 +0800

    支持编辑器退出系统菜单

--- Assets/CozyTown/Unity/Hud/UnityApplicationQuitter.cs
+++ Assets/CozyTown/Unity/Hud/UnityApplicationQuitter.cs
@@ -4,6 +4,13 @@ namespace CozyTown.Unity.Hud
 {
     public sealed class UnityApplicationQuitter : IApplicationQuitter
     {
-        public void Quit() => Application.Quit();
+        public void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
     }
 }
